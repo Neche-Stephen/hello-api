162c515eff26ba34838ad664daee101102e5ee35
Chinecherem <chinecheremubawike@gmail.com>
2024-07-02 17:48:38 +0100

    New changes

--- src/main/java/com/example/helloapi/controller/HelloController.java
+++ src/main/java/com/example/helloapi/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.helloapi.controller;
 
-
+import com.example.helloapi.model.GreetingResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    public GreetingResponse hello(@RequestParam(name = "visitor_name", defaultValue = "Mark") String visitorName) {
+        String clientIp = "127.0.0.1"; // The IP address of the requester
+        String location = "New York"; // The city of the requester
+        String greeting = String.format("Hello, %s! The temperature is 11 degrees Celsius in %s", visitorName, location);
+
+        return new GreetingResponse(clientIp, location, greeting);
     }
 }
